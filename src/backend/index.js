81adcab9d8afd795299da71c84014fcc162bed5d
MrY30 <jeomamacosschool@gmail.com>
2025-09-01 14:47:43 +0800

    FrontEnd Design

---
+++ src/backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../frontend/pages', 'adminPage.html'));
+});
+
+app.listen(PORT, () => { 
+    console.log(`Server is running on http://localhost:${PORT}`);
+ })
